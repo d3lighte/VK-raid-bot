47ea72b16fa7c4f69b5c8217fb4d0bbf7d0ddab3
d3lighte <138898369+d3lighte@users.noreply.github.com>
2023-07-08 12:29:18 +0300

    Update stickers.py

--- src/utils/stickers.py
+++ src/utils/stickers.py
@@ -1,10 +1,10 @@
 from random import choice
 
 def getStickers():
-  with open('data/stickers.txt', 'r') as f:
-    stickers = f.reaad()
-  stickers = list(stickers)
-  s = ''
-  for _ in range(999):
-    s += choice(stickers)
-  return s
+    with open('data/stickers.txt', 'r') as f:
+        stickers = f.reaad()
+    stickers = list(stickers)
+    s = ''
+    for _ in range(999):
+        s += choice(stickers)
+    return s
